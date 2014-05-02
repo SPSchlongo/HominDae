c06235d39be248906aea4d98a6642195a105758c
Richard L <lettrich42@gmail.com>
2014-05-02 16:52:32 -0400

    Update main.rs
    implement stock

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,41 @@ export crate HominDae_Trader;
 
 
 use wxRust::*;
+use rust-html;
 
-struct stockInfo {
-        
-        
+struct stock {
+        name : str
+        price : float32
+        change : float32
+        open : float32
+        close: float32
+        tick : string
+        graph[389] : float 32
+}       // 389 minutres in Trading day, one price for each minute
 
-fn quote -> stockInfo
+
+
+
+
+impl stock
+        fn fromTick (tick :&str) ->  stock {
+                if str.len() > 9 {
+                        stock::error         
+                        }
+                getStock(&tick);
+        }
+        fn search (term :&str) -> stock[] {
+                findStock(&term) 
+        }
+        fn error -> stock {
+                let x[389] = 0;
+                
+                stock { name : "Not Found"
+                        price  : 0.00
+                        change : 0.00
+                        tick   : "N/A"
+                        graph[389] : x
+                }
+        }
+}
+
