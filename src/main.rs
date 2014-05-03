11530cc9ad63e900eb88c63237ab8605e309882f
Richard L <lettrich42@gmail.com>
2014-05-03 12:05:06 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 export crate HominDae_Trader;
 
 
-use wxRust::*;
-use rust-html;
-#![crate_id = "client"]
-
 extern crate http;
 use http::client::RequestWriter;
 use http::method::Get;
@@ -14,52 +10,54 @@ use std::str;
 use std::io::println;
 
 fn main() {
-    println!("started");
-    format!("{}", Get);
-    let args = os::args();
-    if args.len() != 4 {
- 	println!("Usage: {} tick spacing url", args[0]);
-    }
-     else {
-    let url = parse_url(args[1],args[2],args[3]);
-		make_and_print_request(url);
-
-
-            return;
-        }
-    }
-
-
-fn parse_url(tick:&str,spacing:&str,days: &str) -> ~str {
-
-	let x = "http://www.google.com/finance/getprices?q=" + tick + "&i=" + spacing + "&p=" + days + "d";
-	println!("{}",x);
-	x
-	//make_and_print_request(url);
+	println!("started");
+	format!("{}", Get);
+	let args = os::args();
+	if args.len() != 4 {
+	println!("Usage: {} tick spacing url", args[0]);
+	}
+	else {
+	let url = parse_url(args[1],args[2],args[3]);
+	make_and_print_request(url);
+	return;
+	}
 }
 
-fn make_and_print_request(url: &str) {
-    let request: RequestWriter = RequestWriter::new(Get, from_str(url).expect("Invalid URL :-("))
-                                           .unwrap();
-    let mut response = match request.read_response() {
-        Ok(response) => response,
-        Err(_request) => fail!("This example can progress no further with no response :-("),
-    };
+/*
+ *&q is ticker input
+ *&p is number of days
+ *&i is the price every i seconds
+ */
+fn generateURL(tick:&str) -> str {
+	"http://www.google.com/finance/getprices?q=" + tick + "&i=" + "60" + "&p=" + "10" + "d";
+}
 
-    let body = match response.read_to_end() {
-        Ok(body) => body,
-        Err(err) => fail!("Reading response failed: {}", err),
-    };
-    println(str::from_utf8(body.as_slice()).expect("Uh oh, response wasn't UTF-8"));
+fn webRequest(url: &str) -> ~str {
+	let request: RequestWriter = RequestWriter::new(Get, from_str(url).expect("Invalid URL :-(")).unwrap();
+	let mut response = match request.read_response() {
+		Ok(response) => response,
+		Err(_request) => fail!("This example can progress no further with no response :-("),
+	};
+
+	let body = match response.read_to_end() {
+		Ok(body) => body,
+		Err(err) => fail!("Reading response failed: {}", err),
+	};
+	str::from_utf8(body.as_slice()).expect("Uh oh, response wasn't UTF-8")
 }
 
 struct stock {
-        name : str
-        price : float32
-        change : float32
-        open : float32
-        close: float32
-        tick : string
+        name : str,
+        price : float,
+        change : float,
+  	changePer : float
+  	volume : int
+  	ytd : float
+  	ytdPer : float
+        open : float,
+        high : float
+        close : float
+        tick : str,
         graph[389] : float 32
 }       // 389 minutres in Trading day, one price for each minute
 
@@ -67,15 +65,18 @@ struct stock {
 
 
 
-impl stock
+impl stock {
         fn fromTick (tick :&str) ->  stock {
                 if str.len() > 9 {
                         stock::error         
-                        }
-                getStock(&tick);
+                }
+                let URL = generateURL(&tick);
+                let data = webRequest(&URL);
         }
         fn search (term :&str) -> stock[] {
-                findStock(&term) 
+                let URL = generateURL(&str);
+                let data = webRequest(&URL);
+                
         }
         fn error -> stock {
                 let x[389] = 0;
